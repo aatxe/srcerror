c3e1b37740ef392afb901205b526eaab6281717d
doctorsandwich <ernest.chiu2@gmail.com>
2013-11-09 14:41:26 -0500

    Update OnFire.java
    generalize to damage over time

--- ideaprojects/hackromancy/src/main/java/two/hackromancy/core/OnFire.java
+++ ideaprojects/hackromancy/src/main/java/two/hackromancy/core/OnFire.java
@@ -1,17 +1,23 @@
 package two.hackromancy.core;
 public class OnFire extends State{
 	int ticksLeft;
+	int damageDone
 	Organism organism;
+    public OnFire(Organism newOrganism,int ticks, int damage){
+	ticksLeft=ticks;
+	organism=newOrganism;
+	damageDone=damage;
     public OnFire(Organism newOrganism,int ticks){
 	ticksLeft=ticks;
-	organsim=newOrgansim;
+	organism=newOrganism;
+	damageDone=15;
 }
 
     public boolean isRunning(){
 	return (ticksLeft>0)
 }
     public void run(){
-	organism.changeHealth(15);
+	organism.changeHealth(damageDone);
 	ticksLeft--;
 
 }
