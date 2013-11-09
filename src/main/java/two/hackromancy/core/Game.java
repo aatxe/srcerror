51b3cc66c0bac3f3394c067385665555a46b4b29
Aaron Weiss <aaronweiss74@gmail.com>
2013-11-08 21:59:04 -0500

    Added game class with TODOs.

---
+++ src/main/java/two/hackromancy/core/Game.java
@@ -0,0 +1,30 @@
+package two.hackromancy.core;
+
+import two.hackromancy.lwjgl.Window;
+
+/**
+ * @author Aaron
+ * @version 1.0
+ * @since 11/8/13
+ */
+public class Game extends Window {
+	@Override
+	public void init() {
+		// TODO: init
+	}
+
+	@Override
+	public void update(float ticksPassed) {
+		// TODO: game logic
+	}
+
+	@Override
+	public void render() {
+		// TODO: rendering logic
+	}
+
+	@Override
+	public void dispose() {
+		// TODO: clean dat shit up
+	}
+}
