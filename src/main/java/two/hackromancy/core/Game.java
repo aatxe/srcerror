e72b479423cbd53058c76649538d09ed4d96af0f
Aaron Weiss <aaronweiss74@gmail.com>
2013-11-08 22:56:07 -0500

    Added vectors and whatever.

--- src/main/java/two/hackromancy/core/Game.java
+++ src/main/java/two/hackromancy/core/Game.java
@@ -1,6 +1,7 @@
 package two.hackromancy.core;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.GL11;
 import two.hackromancy.lwjgl.Window;
 
 /**
@@ -15,7 +16,8 @@ public class Game extends Window {
 
 	@Override
 	public void init() {
-		// TODO: init
+		GL11.glClearColor(0.4f, 0.6f, 0.9f, 1.0f);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 	}
 
 	@Override
@@ -26,6 +28,17 @@ public class Game extends Window {
 	@Override
 	public void render() {
 		// TODO: rendering logic
+		setupView();
+	}
+
+	public void setupView() {
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, width, 0, height, -1, 1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glLoadIdentity();
+		GL11.glViewport(0, 0, width, height);
+		GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 1);
 	}
 
 	@Override
