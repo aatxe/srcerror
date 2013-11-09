cded2c00b29341108eac245234c54820da4fde60
Strange-Looper <edelman.jd@gmail.com>
2013-11-09 14:58:45 -0500

    get rid of items

--- ideaprojects/hackromancy/src/main/java/two/hackromancy/core/Player.java
+++ ideaprojects/hackromancy/src/main/java/two/hackromancy/core/Player.java
@@ -4,15 +4,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 public class Player extends Organism{
 	private String userName;
-	private ArrayList<Item> items=new ArrayList<Item>();
 	private HashMap<Spell> spells;
 	public Player(int nx, int ny,String nId,String newUserName,HashMap<Spell> nSpells){
 	super(nx,ny,173043,1.0,nId);
 	userName=newUserName;
 	spells=nSpells;
-}
-	public void addItem(Item ni){
-		items.add(ni);
 }
 	public void castSpell(String name){
 	spells.get(name).cast();
@@ -23,14 +19,6 @@ public class Player extends Organism{
 	public void removeSpell(String name){
 	spells.remove(name);
 }
-	public ArrayList<Items> getItems(){
-		return items;
-}
-	public void runItems(){
-		for(Item i:items){
-		if(not(i.run())){items.remove(i);}
-		}
-	}
 
 	public void regen(){
 	changeHealth(10);
