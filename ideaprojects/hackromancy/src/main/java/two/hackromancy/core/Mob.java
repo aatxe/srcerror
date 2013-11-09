99d5d6f1e16c4bc555a806f54d1e79abf6db4a4e
Strange-Looper <edelman.jd@gmail.com>
2013-11-09 16:37:52 -0500

    added attacking and other basics to Mob

--- ideaprojects/hackromancy/src/main/java/two/hackromancy/core/Mob.java
+++ ideaprojects/hackromancy/src/main/java/two/hackromancy/core/Mob.java
@@ -1,10 +1,12 @@
 package two.hackromancy.core;
 import java.util.Math;
 public class Mob extends Organism{
-	Player player;
+	private Player player;
+	private int energy;
 	public Mob(int nx, int ny,String nId,Player newPlayer){
 		super(nx,ny,173043,0.8,nId);
 		player=newPlayer;
+		energy=0;
 	}
      	private ArrayList<Noun> nounsWithinRadius(float radius) {
 
@@ -24,5 +26,17 @@ public class Mob extends Organism{
 	private void move(){
 		changeX(speed*Math.cos(Math.arcos(Math.abs(player.getX() - x)/distToPlayer())));
 		changeY(speed*Math.sin(Math.arcos(Math.abs(player.getX() - x)/distToPlayer())));
+	}
+	private void attack(){
+		if((distToPlayer()<10.0)&&(energy>=100)){
+			player.changeHealth(-25.0);
+			energy=0;
+		}
+	}
+	private boolean run(){
+		energy+=1;
+		move();
+		attack();
+		return (health >= 0.0);
 	}
 }
