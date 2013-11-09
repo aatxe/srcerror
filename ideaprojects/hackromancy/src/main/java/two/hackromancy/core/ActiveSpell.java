a8d536854e22940ff269a1533a96aaccb7709edc
Alok Tripathy <kullalok@gmail.com>
2013-11-09 14:25:02 -0500

    Update ActiveSpell.java
    Organization improvement.

--- ideaprojects/hackromancy/src/main/java/two/hackromancy/core/ActiveSpell.java
+++ ideaprojects/hackromancy/src/main/java/two/hackromancy/core/ActiveSpell.java
@@ -1,19 +1,21 @@
 package two.hackromancy.core;
 
 public class ActiveSpell{
+	
 	private float x;
 	private float y;
 	private float speed;
 	private ArrayList<Noun> nouns;
 	private Player player;
 	private ActivePlayerSpellType spell;
+	
 	public ActiveSpell(ArrayList<Noun> newNouns, Player newPlayer,ActivePlayerSpellType newSpell){
-	player=newPlayer
-	nouns=newNouns;
-	x=player.getX();
-	y=player.getY();
-	spell=newSpell;
-	speed=2.0;
+		player = newPlayer
+		nouns = newNouns;
+		x = player.getX();
+		y = player.getY();
+		spell = newSpell;
+		speed = 2.0;
 	}
 	//add methods like damage which do damage to enerything within a radius
 	public boolean run(){
