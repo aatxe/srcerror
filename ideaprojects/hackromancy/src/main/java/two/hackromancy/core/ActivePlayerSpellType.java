0ab5a5a8507ffc182df1c56035f57e878fcb7c7a
Alok Tripathy <kullalok@gmail.com>
2013-11-09 14:19:39 -0500

    Update ActivePlayerSpellType.java
    Minor changes.

--- ideaprojects/hackromancy/src/main/java/two/hackromancy/core/ActivePlayerSpellType.java
+++ ideaprojects/hackromancy/src/main/java/two/hackromancy/core/ActivePlayerSpellType.java
@@ -1,5 +1,5 @@
 package two.hackromancy.core;
 public interface ActivePlayerSpellType {
-    public boolean run();
-    public String toString();
+    public abstract boolean run();
+    public abstract String toString();
 }
