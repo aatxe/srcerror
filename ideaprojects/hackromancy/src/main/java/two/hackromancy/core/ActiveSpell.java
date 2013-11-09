b372f622714c83b2c0b0b0762c86752ba1fe2e92
Alok Tripathy <kullalok@gmail.com>
2013-11-09 15:53:44 -0500

    Update ActiveSpell.java
    Added getter methods.

--- ideaprojects/hackromancy/src/main/java/two/hackromancy/core/ActiveSpell.java
+++ ideaprojects/hackromancy/src/main/java/two/hackromancy/core/ActiveSpell.java
@@ -2,33 +2,44 @@ package two.hackromancy.core;
 
 public class ActiveSpell{
 	
-	private float x;
-	private float y;
+	private float x, y;
+	private float xVelocity, yVelocity;
 	private float speed;
+	private boolean alive;
 	private ArrayList<Noun> nouns;
 	private Player player;
 	private ActivePlayerSpellType spell;
 	
-	public ActiveSpell(ArrayList<Noun> newNouns, Player newPlayer,ActivePlayerSpellType newSpell){
+	public ActiveSpell(ArrayList<Noun> newNouns, Player newPlayer,ActivePlayerSpellType newSpell, String id){
 		player = newPlayer
 		nouns = newNouns;
 		x = player.getX();
 		y = player.getY();
 		spell = newSpell;
 		speed = 2.0;
+		this.id = id;
 	}
 	
-	private void changeXVelcoity(float velocity) {
+	public void changeXVelcoity(float velocity) {
         	xVelocity += velocity;
     	}
 
-        private void changeYVelocity(float velocity) {
+        public void changeYVelocity(float velocity) {
        	     yVelocity += velocity;
         }
+        
+        public String getID() {
+        	return id;
+        }
+        
+        public boolean isAlive() {
+        	return alive;
+        }
+        
 	//add methods like damage which do damage to enerything within a radius
 	public boolean run(){
 		//if(spell.isDamaging())damage(spell.damage());
-		
+		alive = true;
 		return spell.isAlive();
 	}
 }
