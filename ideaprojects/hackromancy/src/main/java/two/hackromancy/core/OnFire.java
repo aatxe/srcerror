9c1161ee447934a4dd302b1175fbb423c668f772
Alok Tripathy <kullalok@gmail.com>
2013-11-09 14:43:59 -0500

    Update OnFire.java
    Moar organization.

--- ideaprojects/hackromancy/src/main/java/two/hackromancy/core/OnFire.java
+++ ideaprojects/hackromancy/src/main/java/two/hackromancy/core/OnFire.java
@@ -1,25 +1,28 @@
 package two.hackromancy.core;
-public class FireState extends State{
+
+public class OnFire extends State{
 	int ticksLeft;
-	int damageDone;
+	int damageDone
 	Organism organism;
-    public FireState(Organism newOrganism,int ticks, int damage){
-	ticksLeft=ticks;
-	organism=newOrganism;
-	damageDone=damage;
-    public FireState(Organism newOrganism,int ticks){
-	ticksLeft=ticks;
-	organism=newOrganism;
-	damageDone=15;
-}
+    
+    	public OnFire(Organism newOrganism,int ticks, int damage){
+		ticksLeft=ticks;
+		organism=newOrganism;
+		damageDone=damage;
+    	}
+    	
+    	public OnFire(Organism newOrganism,int ticks){
+		ticksLeft=ticks;
+		organism=newOrganism;
+		damageDone=15;
+	}
 
-    public boolean isRunning(){
-	return (ticksLeft>0)
-}
-    public void run(){
-	organism.changeHealth(damageDone);
-	ticksLeft--;
+    	public boolean isRunning(){
+		return (ticksLeft>0)
+	}
+    	public void run(){
+		organism.changeHealth(damageDone);
+		ticksLeft--;
 
-}
-
+	}
 }
