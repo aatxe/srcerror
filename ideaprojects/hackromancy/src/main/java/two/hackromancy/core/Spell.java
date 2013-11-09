3e40be7ad2391aaebf246251e6a3242a5239fe2d
Strange-Looper <edelman.jd@gmail.com>
2013-11-09 14:19:00 -0500

    Create Spell.java

---
+++ ideaprojects/hackromancy/src/main/java/two/hackromancy/core/Spell.java
@@ -0,0 +1,23 @@
+package two.hackromancy.core;
+public class Spell{
+private String spellName;
+private ArrayList<Noun> nouns;
+private Player player;
+public Spell(String newSpellName,Noun newNouns,Player newPlayer){
+	spellName=newSpellName;
+	newPlayer
+}
+public void cast(){//input if needed into here
+	needToMake=true;
+}
+public boolean hasCreate(){
+	return needToMake;
+}
+public ActiveSpell create(){
+	needToMake=false;
+        JythonObjectFactory factory = JythonObjectFactory.getInstance();
+        ActivePlayerSpellType spell = (ActivePlayerSpellType)
+            factory.createObject(ActivePlayerSpellType.class, spellName);
+        return new ActiveSpell(nouns,player,spell);
+}
+}
