f4604d174945a88c0eb61ce43a8976a8105924d4
Strange-Looper <edelman.jd@gmail.com>
2013-11-09 14:41:24 -0500

    have organism implement Noun

--- ideaprojects/hackromancy/src/main/java/two/hackromancy/core/Organism.java
+++ ideaprojects/hackromancy/src/main/java/two/hackromancy/core/Organism.java
@@ -2,7 +2,7 @@ package two.hackromancy.core;
 
 import java.util.ArrayList;
 
-public class Organism {
+public class Organism implements Noun{
         protected float x, y;
         protected String id;
         protected float health;
