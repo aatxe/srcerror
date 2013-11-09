9e7f3e2d726e1e57c8b5831f994bf79f4af76dee
Strange-Looper <edelman.jd@gmail.com>
2013-11-09 11:05:39 -0500

    add content to Organism.java

--- src/main/java/two/hackromancy/core/Organism.java
+++ src/main/java/two/hackromancy/core/Organism.java
@@ -3,83 +3,93 @@ package two.hackromancy.core;
 import java.util.ArrayList;
 
 public class Organism {
-	protected float x, y;
-	protected String id;
-	protected float health;
-	protected float speed;
-	protected ArrayList<State> states = new ArrayList<State>();
+        protected float x, y;
+        protected String id;
+        protected float health;
+	protected float maxHealth;
+        protected float speed;
+        protected ArrayList<State> states = new ArrayList<State>();
+        public Organism(float nx, float ny, int nMaxHealth, float nspeed, String nId) {
+                id = nId;
+                x = nx;
+                y = ny;
+		health=nMaxHealth;
+		maxHealth=nMaxHealth;
+        }
 
-	public Organism(float nx, float ny, int health, float speed, String nId) {
-		id = nId;
-		x = nx;
-		y = ny;
-	}
+        public void left() {
+                x += speed;
+        }
+	public void right() {
+                x -= speed;
+        }
 
-	public void moveX() {
-		x += speed;
-	}
+        public void up() {
+                y += speed;
+        }
+        public void down() {
+                y -= speed;
+        }
+        public void setX(float nx) {
+                x = nx;
+        }
 
-	public void moveY() {
-		y += speed;
-	}
+        public void changeHealth(float ch) {
+                health += ch;
+		if(health>maxHealth)health=maxHealth;
+        }
 
-	public void setX(float nx) {
-		x = nx;
-	}
+        public void setHealth(float nh) {
+                health = nh;
+		if(health>maxHealth)health=maxHealth;
+        }
 
-	public void changeHealth(float ch) {
-		health += ch;
-	}
+        public float getHealth() {
+                return health;
+        }
 
-	public void setHealth(float nh) {
-		health = nh;
-	}
+        public void changeSpeed(float cs) {
+                speed += cs;
+        }
 
-	public float getHealth() {
-		return health;
-	}
+        public void setSpeed(float ns) {
+                speed = ns;
+        }
 
-	public void changeSpeed(float cs) {
-		speed += cs;
-	}
+        public float getSpeed() {
+                return speed;
+        }
 
-	public void setSpeed(float ns) {
-		speed = ns;
-	}
+        public void setY(float ny) {
+                y = cy;
+        }
 
-	public float getSpeed() {
-		return speed;
-	}
+        public float getY() {
+                return y;
+        }
 
-	public void setY(float ny) {
-		y = cy;
-	}
+        public float getX() {
+                return x;
+        }
 
-	public float getY() {
-		return y;
-	}
-
-	public float getX() {
-		return x;
-	}
-
-	public void addState(State ns) {
-
+        public void addState(State ns) {
+		//note that a state should also get the name of the player... I know, but it appears to be the easiest way we can do this
+                //also, even damage should be treated as a state (a one round state)
 		states.add(ns);
-	}
+        }
+}
+        public ArrayList<States> getStates() {
+                return states;
+        }
 
-	public ArrayList<States> getStates() {
-		return states;
-	}
+        public void runStates() {
+                for (State i : states) {
+                        if (!(i.run())) {states.remove(i);}
+                }
+        }
 
-	public void runStates() {
-		for (State i : states) {
-			if (!i.isRunning()) {states.remove(i);}
-		}
-	}
-
-	public boolean run() {
-		runStates();
-		return (health >= 0.0);
-	}
+        public boolean run() {
+                runStates();
+                return (health >= 0.0);
+        }
 }
