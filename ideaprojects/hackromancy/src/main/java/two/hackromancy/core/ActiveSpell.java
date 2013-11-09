f1648f0b10969fd483ce1c56ce8da6d3b21ec399
Alok Tripathy <kullalok@gmail.com>
2013-11-09 16:55:45 -0500

    Update ActiveSpell.java
    Adds function to slow organism down by spell.

--- ideaprojects/hackromancy/src/main/java/two/hackromancy/core/ActiveSpell.java
+++ ideaprojects/hackromancy/src/main/java/two/hackromancy/core/ActiveSpell.java
@@ -44,6 +44,15 @@ public class ActiveSpell{
         	}
     	}	
     
+    	public void slowDown(float radius) {
+
+        	ArrayList<Noun> nouns = nounsWithinRadius(radius);
+        	for(int i = 0; i < nouns.size(); i++) {
+            		if(nouns.get(i) instanceof Organism)
+                		nouns.get(i).addState(new SlowState());
+        	}
+    	}
+    	
         private ArrayList<Noun> nounsWithinRadius(float radius) {
 
         	ArrayList<Noun> nounsInRadius = new ArrayList<Noun>();
