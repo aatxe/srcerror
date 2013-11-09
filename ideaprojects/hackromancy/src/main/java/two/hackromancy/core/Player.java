34ba112728254b76470d1dd43837417deec67bb4
Strange-Looper <edelman.jd@gmail.com>
2013-11-09 16:46:21 -0500

    changed the spells to an ArrayList instead of a HashMap

--- ideaprojects/hackromancy/src/main/java/two/hackromancy/core/Player.java
+++ ideaprojects/hackromancy/src/main/java/two/hackromancy/core/Player.java
@@ -3,37 +3,47 @@ package two.hackromancy.core;
 import java.util.ArrayList;
 import java.util.HashMap;
 public class Player extends Organism{
-	private String userName;
-	private HashMap<Spell> spells;
-	private energy;
-	
-	public Player(int nx, int ny,String nId,String newUserName,HashMap<Spell> nSpells){
-	super(nx,ny,173043,1.0,nId);
-	userName=newUserName;
-	spells=nSpells;
+        private String userName;
+        private ArrayList<Spell> spells;
+        private energy;
+        
+        public Player(int nx, int ny,String nId,String newUserName,ArrayList<Spell> nSpells){
+        super(nx,ny,173043,1.0,nId);
+        userName=newUserName;
+        spells=nSpells;
 }
-	public void castSpell(String name){
-	spells.get(name).cast();
-}
-	public void addSpell(Spell newSpell){
-	spells.put(newSpell.toString(),newSpell);
-}
-	public void removeSpell(String name){
-	spells.remove(name);
-}
-	
-	public void changeEnergy(int delta) {
-		energy += delta;
+        public void castSpell(String name){
+		for(Spell i:spells){
+			if(i.getName().equals(name)){
+				i.cast();
+				break;
+			} 
+		}
 	}
-	public void regen(){
-	changeHealth(10);
-	energy = 10;
+        public void addSpell(Spell newSpell){
+        spells.add(newSpell);
+}
+        public void removeSpell(String name){
+        	for(Spell i:spells){
+			if(i.getName().equals(name)){
+				spells.remove(i);
+				break;
+			} 
+		}
 	}
+        
+        public void changeEnergy(int delta) {
+                energy += delta;
+        }
+        public void regen(){
+        changeHealth(10);
+        changeEnergy(10);
+        }
 
-	public boolean run(){
-		runStates();
-		runItems();
-		regen();
-		return (health >= 0.0);
-	}
+        public boolean run(){
+                runStates();
+                runItems();
+                regen();
+                return (health >= 0.0);
+        }
 }
