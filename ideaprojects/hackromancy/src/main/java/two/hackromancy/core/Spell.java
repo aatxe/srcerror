b461c008d13e4c3ea48d621c3f1833d6b7925431
Strange-Looper <edelman.jd@gmail.com>
2013-11-09 16:43:27 -0500

    add getName to Spell

--- ideaprojects/hackromancy/src/main/java/two/hackromancy/core/Spell.java
+++ ideaprojects/hackromancy/src/main/java/two/hackromancy/core/Spell.java
@@ -2,27 +2,29 @@ package two.hackromancy.core;
 
 public class Spell{
 
-	private String spellName;
-	private ArrayList<Noun> nouns;
-	private Player player;
+        private String spellName;
+        private ArrayList<Noun> nouns;
+        private Player player;
 
-	public Spell(String newSpellName,Noun newNouns,Player newPlayer){
-		spellName=newSpellName;
-		newPlayer
+        public Spell(String newSpellName,Noun newNouns,Player newPlayer){
+                spellName=newSpellName;
+                newPlayer
+        }
+	public String getName(){
+		return spellName;
 	}
-
-	public void cast(){//input if needed into here
-		needToMake=true;
-	}
-	
-	public boolean hasCreate(){
-		return needToMake;
-	}
-	public ActiveSpell create(){
-		needToMake = false;
-        	JythonObjectFactory factory = JythonObjectFactory.getInstance();
-        	ActivePlayerSpellType spell = (ActivePlayerSpellType)
-             	factory.createObject(ActivePlayerSpellType.class, spellName);
-        	return new ActiveSpell(nouns,player,spell);
-	}
+        public void cast(){//input if needed into here
+                needToMake=true;
+        }
+        
+        public boolean hasCreate(){
+                return needToMake;
+        }
+        public ActiveSpell create(){
+                needToMake = false;
+                JythonObjectFactory factory = JythonObjectFactory.getInstance();
+                ActivePlayerSpellType spell = (ActivePlayerSpellType)
+                     factory.createObject(ActivePlayerSpellType.class, spellName);
+                return new ActiveSpell(nouns,player,spell);
+        }
 }
