3e51d9ad7886cc30a2ce000967e96feea50b9403
Alok Tripathy <kullalok@gmail.com>
2013-11-09 16:01:56 -0500

    Update Noun.java
    Added abstract method to get Vector.

--- ideaprojects/hackromancy/src/main/java/two/hackromancy/core/Noun.java
+++ ideaprojects/hackromancy/src/main/java/two/hackromancy/core/Noun.java
@@ -3,5 +3,6 @@ public abstract class Noun{
 	public abstract float getX();
 	public abstract float getY();
 	public abstract String getId();
+	public abstract Vector getVector();
 	public abstract boolean run();
 }
