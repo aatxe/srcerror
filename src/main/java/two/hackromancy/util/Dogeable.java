f1969579759899e08d88dd9c754fa4e0190a8d95
Aaron Weiss <aaronweiss74@gmail.com>
2013-11-08 23:02:55 -0500

    Added dogeable interface.

---
+++ src/main/java/two/hackromancy/util/Dogeable.java
@@ -0,0 +1,17 @@
+package two.hackromancy.util;
+
+/**
+ * @author Aaron
+ * @version 1.0
+ * @since 11/8/13
+ */
+public interface Dogeable {
+	/**
+	 * wow
+	 * very programming
+	 * so encapsulated
+	 * much interface
+	 * wow
+	 */
+	public void doge();
+}
