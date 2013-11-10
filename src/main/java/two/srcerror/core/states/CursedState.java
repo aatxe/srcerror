4c6bfce851bfbe106e63b5bc1060a07600ee2c6c
Alok Tripathy <kullalok@gmail.com>
2013-11-10 03:09:55 -0500

    Created state of being cursed.

---
+++ src/main/java/two/srcerror/core/states/CursedState.java
@@ -0,0 +1,29 @@
+package two.srcerror.core.states;
+
+import two.srcerror.core.State;
+import two.srcerror.core.nouns.Organism;
+import two.srcerror.util.Constants;
+
+public class CursedState implements State {
+
+    private final Organism organism;
+    private final int modifier;
+    private int remainingTicks;
+
+    public CursedState(Organism organism, int length, int modifier) {
+        this.organism = organism;
+        remainingTicks = length;
+        this.modifier = modifier; 
+    }
+
+    @Override
+    public boolean isActive() {
+        return (remainingTicks > 0);
+    }
+
+    @Override
+    public void tick() {
+        organism.hurt(modifier * Constants.CURSE_TICK_DAMAGE);
+        remainingTicks--;
+    }
+}
