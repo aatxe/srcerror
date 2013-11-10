020b8f256dbab2e7fe56b8bfe04fa741ccba6bf0
Alok Tripathy <kullalok@gmail.com>
2013-11-09 21:57:46 -0500

    Update Mob.java

--- src/main/java/two/hackromancy/core/Mob.java
+++ src/main/java/two/hackromancy/core/Mob.java
@@ -5,18 +5,29 @@ import java.util.ArrayList;
 public class Mob extends Organism {
 	private Player player;
 	private int energy;
-
-	public Mob(int nx, int ny, String nId, Player newPlayer) {
-		super(nx, ny, 173043, 0.8, nId);
+	private ArrayList<Noun> worldNouns;
+	private Vector vector;
+	
+	public Mob(ArrayList<Noun> nouns, int nx, int ny, String nId, Player newPlayer) {
+		super(nx, ny, 173043, 0.8f, nId);
+		worldNouns = nouns;
 		player = newPlayer;
 		energy = 0;
+		vector = new Vector(0);
 	}
 
+	public String getId() {
+		return super.id;
+	}
+	
+	public Vector getVector() {
+		return vector;
+	}
 	private ArrayList<Noun> nounsWithinRadius(float radius) {
 		ArrayList<Noun> nounsInRadius = new ArrayList<Noun>();
 		for (int i = 0; i < worldNouns.size(); i++) {
 			Noun curr = worldNouns.get(i);
-			float dist = Math.sqrt((curr.getX() - x) * (curr.getX() - x) + (curr.getY() - y) * (curr.getY() - y));
+			float dist = (float)Math.sqrt((curr.getX() - x) * (curr.getX() - x) + (curr.getY() - y) * (curr.getY() - y));
 			if (dist <= radius) nounsInRadius.add(curr);
 		}
 
@@ -24,23 +35,31 @@ public class Mob extends Organism {
 	}
 
 	private float distToPlayer() {
-		return Math.sqrt((player.getX() - x) * (player.getX() - x) + (player.getY() - y) * (player.getY() - y));
+		return (float)Math.sqrt((player.getX() - x) * (player.getX() - x) + (player.getY() - y) * (player.getY() - y));
 
 	}
 
 	private void move() {
-		changeX(speed * Math.cos(Math.arcos(Math.abs(player.getX() - x) / distToPlayer())));
-		changeY(speed * Math.sin(Math.arcos(Math.abs(player.getX() - x) / distToPlayer())));
+		changeX(speed * (float)Math.cos(Math.acos(Math.abs(player.getX() - x) / distToPlayer())));
+		changeY(speed * (float)Math.sin(Math.acos(Math.abs(player.getX() - x) / distToPlayer())));
 	}
 
+	private void changeX(float newX) {
+        	super.setX(newX);
+    	}
+    
+    	private void changeY(float newY) {
+        	super.setY(newY);
+    	}
+    	
 	private void attack() {
 		if ((distToPlayer() < 10.0) && (energy >= 100)) {
-			player.changeHealth(-25.0);
+			player.changeHealth(-25.0f);
 			energy = 0;
 		}
 	}
 
-	private boolean run() {
+	public boolean run() {
 		energy += 1;
 		move();
 		attack();
