fe5027fe35bc8c63a8212bf4f096ce84c5ead6c4
Alok Tripathy <kullalok@gmail.com>
2013-11-10 03:22:26 -0500

    Added constant for RegenState.

--- src/main/java/two/srcerror/util/Constants.java
+++ src/main/java/two/srcerror/util/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
 	public static final float ACCELERATION = 1.0f;
 	public static final int FIRE_TICK_DAMAGE = 2;
 	public static final int CURSE_TICK_DAMAGE = 3;
+	public static final int REGEN_TICK_HEALTH = 2;
 }
