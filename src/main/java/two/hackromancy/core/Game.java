e8f8848f0b29a4274e678441453c2b4b5e4e0ba0
Alok Tripathy <kullalok@gmail.com>
2013-11-09 22:47:33 -0500

    Update Game.java

--- src/main/java/two/hackromancy/core/Game.java
+++ src/main/java/two/hackromancy/core/Game.java
@@ -23,21 +23,31 @@ public class Game {
 	}
 
 	public ArrayList<Spell> getSpells(final File folder) {
-		ArrayList<Spell> ret = new ArrayList<Spell>();
-		for (final File fileEntry : folder.listFiles()) {
-			if (fileEntry.isDirectory()) {
-				listFilesForFolder(fileEntry);
-			} else {
-				if (fileEntry.getName().substring(fileEntry.getName().length() - 3, fileEntry.getName().length()).equals(".py")
-				{
-					String newSpellName = fileEntry.getName().substring(0, fileEntry.getName().length() - 3);
-					ret.add(new Spell(newSpellName, nouns, player));
-				}
 
-			}
-		}
-		return ret;
-	}
+        	ArrayList<File> pythonFiles = getSpellFiles(folder);
+        	ArrayList<Spell> ret = new ArrayList<Spell>();
+
+        	for(final File fileEntry : pythonFiles) {
+            		if(fileEntry.getName().substring(fileEntry.getName().length() - 3, fileEntry.getName().length()).equals(".py")) {
+                		String newSpellName = fileEntry.getName().substring(0, fileEntry.getName().length() - 3);
+                		ret.add(new Spell(newSpellName, nouns, player));
+            		}
+        	}
+
+        	return ret;
+    
+    	}
+	
+	private ArrayList<File> getSpellFiles(final File folder) {
+        	for (final File fileEntry : folder.listFiles()) {
+            		if (fileEntry.isDirectory()) {
+                		getSpellFiles(fileEntry);
+            		} 
+            		else {
+                		files.add(fileEntry);
+            		}
+        	}
+    	}
 
 	public void update(float ticksPassed) {
 		for (int i = 0; i<ticksPassed; i++){
