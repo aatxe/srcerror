e036b4279643926ff2d7a93237514df1e4c7ee17
Alok Tripathy <kullalok@gmail.com>
2013-11-10 03:10:41 -0500

    Added CURSE_TICK_DAMAGE constant.

--- src/main/java/two/srcerror/util/Constants.java
+++ src/main/java/two/srcerror/util/Constants.java
@@ -3,4 +3,5 @@ package two.srcerror.util;
 public class Constants {
 	public static final float ACCELERATION = 1.0f;
 	public static final int FIRE_TICK_DAMAGE = 2;
+	public static final int CURSE_TICK_DAMAGE = 3;
 }
